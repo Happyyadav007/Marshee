fb58e7153747b63f676996d8d90bdb15d5c9e19a
happy <hy249796@gmail.com>
2025-08-10 18:22:17 +0530

    changes for deployment, vercel.json added

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://marshee.onrender.com/api',
-//   baseURL: 'http://localhost:3000/api',
+    // baseURL: 'https://marshee.onrender.com/api',
+  baseURL: 'http://localhost:3000/api',
 //   baseURL: 'http://localhost:4000/api',
 //   withCredentials: true
 });
